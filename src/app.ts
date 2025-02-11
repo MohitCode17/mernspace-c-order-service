eed410da6a0e1cdd6f5e5647caf92e6fb2aae237
MohitCode17 <mohit.codes17@gmail.com>
2025-02-11 18:52:57 +0530

    add cors

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,16 @@ import { globalErrorHandler } from "./common/middleware/globalErrorHandler";
 import cookieParser from "cookie-parser";
 import customerRouter from "./customer/customer-router";
 import couponRouter from "./coupon/coupon-router";
+import cors from "cors";
 
 const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:5173"],
+    credentials: true,
+  }),
+);
+
 app.use(cookieParser());
 app.use(express.json());
 
