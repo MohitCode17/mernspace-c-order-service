0d6bcf55ebadfaf348fa77df19b692c5b88173b7
MohitCode17 <mohit.codes17@gmail.com>
2025-02-14 23:09:32 +0530

    calculate discount

--- src/order/order-controller.ts
+++ src/order/order-controller.ts
@@ -7,12 +7,28 @@ import {
 } from "../types";
 import productCacheModel from "../productCache/productCacheModel";
 import toppingCacheModel from "../toppingCache/toppingCacheModel";
+import couponModel from "../coupon/coupon-model";
 
 export class OrderController {
   createOrder = async (req: Request, res: Response) => {
     // TODO: VALIDATE REQUEST DATA
     const totalPrice = await this.calculateTotal(req.body.cart);
-    return res.json({ totalPrice });
+
+    // CALCULATE DISCOUNT
+    let discountPercentage = 0;
+    const couponCode = req.body.couponCode;
+    const tenantId = req.body.tenantId;
+
+    if (couponCode) {
+      discountPercentage = await this.getDiscountPercentage(
+        couponCode,
+        tenantId,
+      );
+    }
+
+    const discountAmount = Math.round((totalPrice * discountPercentage) / 100);
+
+    return res.json({ discountAmount });
   };
 
   private calculateTotal = async (cart: CartItem[]) => {
@@ -98,5 +114,25 @@ export class OrderController {
     }
 
     return currentTopping.price;
+  };
+
+  private getDiscountPercentage = async (
+    couponCode: string,
+    tenantId: string,
+  ) => {
+    const code = await couponModel.findOne({ code: couponCode, tenantId });
+
+    if (!code) {
+      return 0;
+    }
+
+    const currentDate = new Date();
+    const couponDate = new Date(code.validUpto);
+
+    if (currentDate <= couponDate) {
+      return code.discount;
+    }
+
+    return 0;
   };
 }
