da5716c96fa9dc9b12b4953bf92603ab7ec6e8fc
MohitCode17 <mohit.codes17@gmail.com>
2025-02-18 20:58:53 +0530

    change expire time of idempotency-key

--- src/idempotency/idempotency-model.ts
+++ src/idempotency/idempotency-model.ts
@@ -14,8 +14,7 @@ const idempotencySchema = new mongoose.Schema(
   { timestamps: true },
 );
 
-// TODO: FIX EXPIRES TIME TO 48 HOURS (e.g. - 60 * 60 * 48)
-idempotencySchema.index({ createdAt: 1 }, { expireAfterSeconds: 20 });
+idempotencySchema.index({ createdAt: 1 }, { expireAfterSeconds: 60 * 60 * 48 });
 idempotencySchema.index({ key: 1 }, { unique: true });
 
 export default mongoose.model("Idempotency", idempotencySchema);
